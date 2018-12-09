9117119dce6cc10ae8e503ade1e6fae1fffeadf2
Marisa Smith <priparadigm@gmail.com>
2018-12-08 23:49:56 -0800

    can open the file

--- 01--chronal-calibration/chronal-calibration.py
+++ 01--chronal-calibration/chronal-calibration.py
@@ -4,7 +4,11 @@
 
 
 # initialize variables
-
+total = 0
+testFile1 = "test1.txt"
+testFile2 = "test2.txt"
+testFile3 = "test3.txt"
+realFile = "input.txt"
 
 # get data
 
@@ -13,4 +17,24 @@
 
 
 # display results
-print("hello")
+
+
+def totalFile(filename):
+    try:
+        currentFile = open(filename, 'r')
+        return 1
+    except FileNotFoundError:
+        print("\nError: File not found: ", filename)
+        return None
+
+
+    currentFile.close()
+
+
+def main():
+    answer = totalFile(realFile)
+    if answer:
+        print("The total is %i." %(answer))
+
+
+main()
